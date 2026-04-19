e7bfaf4168134683102989eac61aa14fd87cbcbd
KanashiXI <panyapat.test@gmail.com>
2026-04-19 18:07:32 +0700

    edit route charWeapon

--- src/modules/character/weapon/route.js
+++ src/modules/character/weapon/route.js
@@ -5,7 +5,7 @@ const router = Router();
 
 router.get("/charWeapons", charWeaponsController.getAllCharWeapons); 
 router.post("/charWeapons", charWeaponsController.addListCharWeapons);
-router.put("/charWeapons/:id", charWeaponsController.editCharWeapon);
-router.delete("/charWeapons/:id", charWeaponsController.deleteCharWeapon);
+router.put("/charWeapon/:id", charWeaponsController.editCharWeapon);
+router.delete("/charWeapon/:id", charWeaponsController.deleteCharWeapon);
 
 export default router;
